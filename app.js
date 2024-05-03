b81a013b0aa6d1d0cdf1f093cbd282cd74880454
kalon <akandeoluwanifemi@gmail.com>
2024-05-03 14:13:08 +0100

    commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
+require("express-async-errors");
 
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleWare = require("./middleware/error-handler");
@@ -12,6 +14,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send('<h1>Store api</h1><a href="/api/v1/products">Products route<a/>');
 });
+
+app.use("/api/v1/products", productsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleWare);
