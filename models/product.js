fdfafd1a0f6930647c1603c8c57e27cdfff90ce4
kalon <akandeoluwanifemi@gmail.com>
2024-05-03 16:39:13 +0100

    add origin

---
+++ models/product.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "product name must be provided"],
+  },
+  price: {
+    type: Number,
+    required: [true, "product price must be provided"],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: string,
+    enum: ["Ikea", "liddy", "caressa", "marcos"],
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
