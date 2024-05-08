c08780e5427102c600580331638795cf913ae005
kalon <akandeoluwanifemi@gmail.com>
2024-05-08 16:04:12 +0100

    update the controllers

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,21 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "Products testing routes-static" });
+  const products = await Product.find({ name: "vase table" });
+  res.status(200).json(products);
 };
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Products testing routes" });
+  const { featured, company } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  const products = await Product.find(req.query);
+  res.status(200).json({
+    products,
+    nbHits: products.length,
+  });
 };
 
 module.exports = {
